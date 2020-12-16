50a51810b95bc7caece28c49555fc48c6dd896e8
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-17 05:13:11 +0700

    - math.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import add, { substract } from './math';
-
-console.log(add(1, 5));
-console.log(substract(6, 3));
--- src/math.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => {
-    return `${a} + ${b} = `+ (a + b);
-}
-
-const substract = (a, b) => {
-    return `${a} - ${b} = `+ (a - b);
-}
-
-export { add as default, substract };
