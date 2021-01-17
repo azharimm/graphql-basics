eda17cb8302ada83f7c5fcd8b0ae0dbca03a61f9
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-17 10:06:01 +0700

    ^ src/index: spread operator on args

--- src/index.js
+++ src/index.js
@@ -116,9 +116,7 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
 
             users.push(user);
@@ -134,10 +132,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
 
             posts.push(post);
@@ -158,9 +153,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
 
             comments.push(comment);
