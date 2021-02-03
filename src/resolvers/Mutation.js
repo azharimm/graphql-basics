ca7957b123db1a19925d1d5bde49380aa5144c9f
Azhari Marzan <azhari.marzan@gmail.com>
2021-02-04 05:23:11 +0700

    createPost subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -63,7 +63,7 @@ const Mutation = {
 
         return user;
     },
-    createPost(parent, args, { db }, info) {
+    createPost(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some(user => user.id === args.data.author);
 
         if(!userExists) {
@@ -76,6 +76,9 @@ const Mutation = {
         }
 
         db.posts.push(post);
+        if(args.data.published) {
+            pubsub.publish('post', { post }); //subscription
+        }
 
         return post;
     },
